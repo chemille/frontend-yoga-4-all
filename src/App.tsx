567c9800cd3920e46923d79ccc7b75f7d1747cf5
Michelle Oh <ohmotrl@gmail.com>
2023-02-02 12:07:10 -0500

    added axios call to Card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-
 import { NavBar } from "./Navbar";
 import { Reflex } from "./pages/Reflex";
 import { MyAccount } from "./pages/MyAccount";
 import { Diary } from "./pages/Diary";
 
+
 export function App() {
+
   return (
     <>
       <BrowserRouter>
