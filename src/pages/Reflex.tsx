41e45a694de2edb3561cecd7e2cf589a303f520e
Michelle Oh <ohmotrl@gmail.com>
2023-02-03 14:44:39 -0500

    Co-authored-by: Farrah-Unger <Farrah-Unger@users.noreply.github.com>

--- src/pages/Reflex.tsx
+++ src/pages/Reflex.tsx
@@ -1,30 +1,15 @@
 import Home from "../components/Home";
 // import axios from "axios";
 // import { useState, useEffect } from "react";
-import downwardDog from "./resources/downwardDog.jpg";
-
-// export type oneReflex = {
-//     reflex_id: number;
-//     title: string;
-//     education: string;
-//     videos: Array<string>;
-// };
+// import downwardDog from "./resources/downwardDog.jpg";
     
 export function Reflex() {
-    // const [oneReflex, setOneReflex] = useState<oneReflex>();
-    // useEffect(() => {
-    //     const url = `https://yoga-4-all-backend.herokuapp.com/${oneReflex}`;
-    //     axios.get(url).then((response) => {
-    //         setOneReflex(response.data);
-    //     });
-    // }, []);
-    // console.log(oneReflex);
 
     return (
         <div>
             <h1>Reflex</h1>
         <div> 
-        <img src={downwardDog} alt=""/>
+        {/* <img src={downwardDog} alt=""/> */}
         </div>
         </div>
     );
