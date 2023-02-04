644596942e5da926e24789bf912a3a31e764e9f9
Michelle Oh <ohmotrl@gmail.com>
2023-02-04 13:00:09 -0500

    updated DiaryEntries component

--- src/components/DiaryEntries.tsx
+++ src/components/DiaryEntries.tsx
@@ -1,1 +1,10 @@
 // do we make the axios calls for submitting the diary form here?
+import React from 'react'
+
+const DiaryEntries = () => {
+    return (
+    <div>DiaryEntries</div>
+    )
+}
+
+export default DiaryEntries
