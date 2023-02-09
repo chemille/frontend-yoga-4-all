7b41584910692a7936d179d47f90c818b357d57e
Michelle Oh <ohmotrl@gmail.com>
2023-02-09 16:57:18 -0500

    added userlogin styling

--- src/components/UserLogin.tsx
+++ src/components/UserLogin.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import './userLoginStyles.css';
 
 const UserLogin: any = () => {
     const [email, setEmail] = useState("");
@@ -9,29 +10,33 @@ const UserLogin: any = () => {
         // console.log({email, password});
     };
 
-    // const [values, handleChange, handleSubmit] = useForm(login);
-
-
     return (
-        <form onSubmit={login}>
-            <label>Email:</label>
-            <input
-                value={email}
-                id='email'
-                type='text'
-                placeholder='Email'
-                onChange={e => setEmail(e.target.value)}
-                />
-            <label>Password:</label>
-            <input
-                value={password}
-                id='password'
-                type='text'
-                placeholder='Password'
-                onChange={e => setPassword(e.target.value)}
-                />
-            <button type='submit'>Login</button>
-        </form>
+        <div>
+            <h2>Login to your account</h2>
+            <br></br>
+            <form onSubmit={login}>
+                <label>Email:</label>
+                <input className="input input__box"
+                    value={email}
+                    id='email'
+                    type='text'
+                    placeholder='Email'
+                    onChange={e => setEmail(e.target.value)}
+                    />
+                <br></br>
+                <label>Password:</label>
+                <input className="input input__box"
+                    value={password}
+                    id='password'
+                    type='text'
+                    placeholder='Password'
+                    onChange={e => setPassword(e.target.value)}
+                    />
+                    <br></br>
+                <button className="input_submit" type='submit'>Login</button>
+                <button className="signup_btn" type='submit'>Sign up</button>
+            </form>
+        </div>
     );
 }    
 
